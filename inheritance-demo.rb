975f94d1b480775c370fafd37a2017d0a3887245
jordanoboyle <jmcmcode@gmail.com>
2024-05-08 11:42:54 -0600

    added class Transportation-mode, eliminated repeats in class Bike and class Car

---
+++ inheritance-demo.rb
@@ -0,0 +1,33 @@
+
+# I want to create a class these can take traits from
+class Transportation
+  def initialize
+    @speed     = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+
+class Car
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike
+  def ring_bell
+    puts "Ring ring!"
+  end
+end

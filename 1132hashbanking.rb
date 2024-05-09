62210581a05f13d720697b6daf89af5405db21b1
jordanoboyle <jmcmcode@gmail.com>
2024-05-08 22:07:16 -0600

    can display client information in block form

--- 1132hashbanking.rb
+++ 1132hashbanking.rb
@@ -21,8 +21,14 @@ end
 # wrap the infor in a has and return
 # push the hash to the bankers array
 # need to create loop to add 5 clients. times loop will do, but this could easily rip through a data base as well with an adjustment
+temp_clients = [
+  {first_name: "Jules", last_name: "Run The", email_address: "atlantahellyeah@rtj.com"},
+  {first_name: "James", last_name: "Bonds", email_address: "bond@mi6.com"},
+  {first_name: "Jack", last_name: "Sparrow", email_address: "sparrow@blackpearl.com"},
+]
 
-def add_account_number()
+def add_account_number(client_info_array) # first_name, last_name, email_address
+  clients_account_info = []
   def acct_num_generator
     rand_acct_array = []
     10.times do
@@ -30,12 +36,33 @@ def add_account_number()
     end
     return rand_acct_array.join('')
   end
-  return acct_num_generator()
+  client_info_array.each do |info|
+    info[:account_num] = acct_num_generator()
+    clients_account_info << info
+  end
+  return clients_account_info
 end
 
-p add_account_number
+# p add_account_number(temp_clients)
 
 # method works.
 #return random number from inner method
 #return random ten digit number
 
+#lets make sure we can access stuff from the hash pulled
+# can we loop and print one piece of info?
+
+def display_client_info(client_account_hash) # :first_name :last_name :email_address :account_num
+  client_account_hash.each do |info| 
+    p "FIRST NAME: #{info[:first_name]}"
+    p "LAST NAME:  #{info[:first_name]}"
+    p "EMAIL:      #{info[:first_name]}"
+    p "ACCT #:     #{info[:first_name]}"
+  end
+
+end
+
+display_client_info(temp_clients)
+
+
+# print out the information of client in a loop

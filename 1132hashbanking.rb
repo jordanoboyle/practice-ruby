fdc0e172b5fa64b051c442ecf0a0482b0db293c4
jordanoboyle <jmcmcode@gmail.com>
2024-05-08 21:29:15 -0600

    add_banker complete and scalable

--- 1132hashbanking.rb
+++ 1132hashbanking.rb
@@ -1,18 +1,25 @@
 
 
-def add_depositor
+def add_depositor # can be modified to rip through a data base
   bankers = []
-
-  puts "Enter first name:"
-  first_name = gets.chomp.capitalize
-  puts "Enter last name"
-  last_name = gets.chomp.capitalize
-  puts "Enter email: "
-  email_address = gets.chomp
-
-  return first_name, last_name
+  5.times do
+    puts "Enter first name:"
+    first_name = gets.chomp.capitalize
+    puts "Enter last name"
+    last_name = gets.chomp.capitalize
+    puts "Enter email: "
+    email_address = gets.chomp
+    bankers << {first_name: first_name, last_name: last_name, email_address: email_address}
+  end
+  return bankers
 end
 
-puts add_depositor
+p add_depositor
 
-# write prompts to get first_name, last_name and email and return hash
+# write prompts to get first_name, last_name and email
+# wrap the infor in a has and return
+# push the hash to the bankers array
+# need to create loop to add 5 clients. times loop will do, but this could easily rip through a data base as well with an adjustment
+
+
+

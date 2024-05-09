6ea2f6987ccca87547cef71d5e584305a32565e1
jordanoboyle <jmcmcode@gmail.com>
2024-05-08 20:42:18 -0600

    favorite_food method, returns array of fav foods

--- 113VarsArrayLoops.rb
+++ 113VarsArrayLoops.rb
@@ -1,11 +1,21 @@
+def favorite_food
+  fav_foods = []
 
-fav_foods = []
-
-while true
-  if fav_foods.length == 5
-    break
-  elsif fav_foods.length < 1
-    puts "This should print when run"
-    break
+  while true
+    if fav_foods.length < 1
+      puts "What is a favorite food of yours?"
+      first_food_input = gets.chomp
+      fav_foods << first_food_input
+    elsif fav_foods.length < 5
+      puts "What is another favorite of yours?"
+      subsequent_food_input = gets.chomp
+      fav_foods << subsequent_food_input
+    else
+      break
+    end
   end
+  return fav_foods
 end
+
+p favorite_food()
+

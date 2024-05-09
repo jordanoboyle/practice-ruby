d50f922069af67cb440fd2cf613277d11cac4ad7
jordanoboyle <jmcmcode@gmail.com>
2024-05-08 21:41:44 -0600

    random number generator

--- 1132hashbanking.rb
+++ 1132hashbanking.rb
@@ -14,12 +14,28 @@ def add_depositor # can be modified to rip through a data base
   return bankers
 end
 
-p add_depositor
+
+# p add_depositor
 
 # write prompts to get first_name, last_name and email
 # wrap the infor in a has and return
 # push the hash to the bankers array
 # need to create loop to add 5 clients. times loop will do, but this could easily rip through a data base as well with an adjustment
 
+def add_account_number()
+  def acct_num_generator
+    rand_acct_array = []
+    10.times do
+      rand_acct_array << rand(0...10)
+    end
+    return rand_acct_array.join('')
+  end
+  return acct_num_generator()
+end
 
+p add_account_number
+
+# method works.
+#return random number from inner method
+#return random ten digit number
 
